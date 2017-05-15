80929d1a595e4b665bc849503704dd0737c03516
hagenheermann <daniel.herman@gmx.de>
2017-05-15 15:32:29 +0200

    INIT

---
+++ main.js
@@ -0,0 +1,251 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var exphbs = require('express-handlebars');
+var flash = require('connect-flash');
+var session = require('express-session');
+var validator = require('express-validator');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bodyParser = require('body-parser');
+var userdbmodule = require('./userdbmodule');
+var mime = require('mime');
+var fs = require('fs');
+const fileUpload = require('express-fileupload');
+const __FilesDir = './Files/';
+
+
+
+/*Creating express app obj */
+var app = express();
+
+/*Creating parser obj */
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
+/*Const setups */
+const port = 8888;
+
+/* Setting up express */
+app.use(express.static(path.join( __dirname,'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(validator());
+app.use(fileUpload());
+
+/*Set up for handlebars engine */
+app.engine('handlebars',exphbs({defaultLayout:'layout'}));
+app.set('view engine','handlebars');
+
+/* Session set up */
+app.use(session({
+    secret:'secret',
+    saveUninitialized:true,
+    resave:true
+}));
+
+/* Set up for the validator */
+app.use(validator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+/* Set up for flash */
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+/*Setting up db */
+var userdb = userdbmodule.connectDB();
+//only run the statement below when you havent set up the tables already
+userdbmodule.createTables(userdb) 
+//userdbmodule.dropUserTable(userdb);
+
+/* HTTP function stuff */
+
+/* Login */
+function LoginResponse(req,resp)
+{
+    resp.render('login');
+    console.log("login html request")
+}
+app.get('/',function(req,resp){LoginResponse(req,resp);});
+app.get('',function(req,resp){LoginResponse(req,resp);})
+
+app.post('/login_post',urlencodedParser,function(req,resp){
+    user = {
+        u_name:req.body.username,
+        u_pass:req.body.password 
+    }
+
+    req.checkBody('username','Username is required').notEmpty();
+    req.checkBody('password','Password is required').notEmpty();
+
+    var errors = req.validationErrors();
+    
+    if(errors)
+    {
+        console.log("errors occured");
+        resp.render('login',{
+            errors:errors
+        });
+    }
+    else
+    {
+        userdbmodule.handleLogin(userdb,user.u_name,user.u_pass,req,resp);
+        //console.log(val);
+        //resp.render('mainpage');
+    }
+})
+
+/*Registration */
+
+app.post('/registration_post',function(req,resp){
+    
+    /*Creating a json format obj for the registration credentials*/
+    credentials = {
+        u_name:req.body.username,
+        u_password_0:req.body.password0,
+        u_password_1:req.body.password1,
+    } 
+    
+    /* Validations */
+    req.checkBody('username','Username is required').notEmpty();
+    req.checkBody('password0','Password is required').notEmpty();
+    req.checkBody('password1','Password repeat is required').notEmpty();
+    req.checkBody('password1','Passwords do not match').equals(req.body.password0);
+
+    var errors = req.validationErrors();
+
+    if(errors)
+    {
+        console.log("errors occured");
+        resp.render('login',{
+            errors:errors
+        })
+    }
+    else
+    {
+        userdbmodule.handleRegister(userdb,credentials.u_name,credentials.u_password_0,req,resp);
+    }
+})
+
+/* LEGAL */
+app.get('/Legal',function(req,resp)
+{
+    resp.render('legal');
+})
+
+/* ABOUT */
+app.get('/About',function(req,resp)
+{
+    resp.render('about');
+})
+
+/* File Upload */
+function file_listing()
+{
+    var filenames = [];
+    fs.readdir(__FilesDir,function(err,files){
+        files.forEach(file=>{
+            filenames.push(file);
+        })
+    });
+    return filenames;
+}
+
+
+app.post('/file_upload',function(req,resp){
+   
+   if(!req.files)
+   {
+       var uploaderr = "No filen given!";
+       var filenames = file_listing();
+       resp.render('mainpage',{uploaderr,filenames})
+   }
+   else
+   {
+       let uploaded_file = req.files.file;
+       var filename = req.files.file.name;
+       var file_path = path.join(__dirname,"Files",filename);
+
+       console.log(filename);
+       console.log(file_path);
+
+        uploaded_file.mv(file_path,function(err)
+        {
+            if(err)
+            {
+                var uploaderr = "Error while uploading file!";
+                var filenames = file_listing();
+                resp.render('mainpage',{uploaderr,filenames});
+                console.log("Error moving the file to the Files directory");
+            }
+            else
+            {
+                var filenames = file_listing();
+                resp.render('mainpage',{filenames});
+                console.log("Succesfull upload");
+            }
+        });
+    }
+
+    
+})
+
+/* Download GET */
+app.get('/download',function(req,resp)
+{
+    /* Check if the user has selected a file for its request */
+    req.checkBody('filename',"No file selected").notEmpty();
+    var errors = req.validationErrors();
+    
+    if(errors)
+    {
+        /* Rendering the mainpage again with the error mesages */
+        var filenames = file_listing();
+        resp.render('mainpage',{errors,filenames});
+    }
+    else
+    {
+        /* Getting all the infos for the piping */
+        var filename = req.body.filename;
+        var file = path.join(__dirname,"Files",filename);
+        var mimetype = mime.lookup(file);
+        
+        /* Setting the headers for the browser */
+        resp.setHeader('Content-disposition','attachment; filename='+filename);
+        resp.setHeader('Content-type',mimetype);
+
+        /* creating the file stream for the response and starting the piping */
+        var stream = fs.createReadStream(file);
+        stream.pipe(resp);
+    }
+})
+
+
+/* Listen start */
+var server = app.listen(port,function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+
+    console.log("Example app listening at http://%s:%s",host,port);
+})
---
+++ userdbmodule.js
@@ -0,0 +1,258 @@
+/*
+* Table users:
+* username TEXT,
+* password TEXT
+*
+* Table files:
+* filename TEXT,
+* filetype TEXT,
+* path TEXT,
+* filesize NUM <- später
+*
+*/
+
+var sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
+const __FilesDir = './Files/';
+
+module.exports = 
+{
+    
+    
+    //DONE
+    connectDB:function()
+    {
+        var db = new sqlite3.Database('userdb');
+        console.log("userdbmodule: Created connection to the user db");
+        return db;
+    },
+    
+    //DONE
+    createTables:function(DBobj)
+    {
+        var user_table_query = "CREATE TABLE users(username TEXT,password TEXT)";
+        var files_table_query = "CREATE TABLE files(filename TEXT, filetype TEXT, path TEXT)";
+        DBobj.run(user_table_query,function(err){
+            if(err)
+            {
+                console.log("userdbmodule: Table already created");
+            }else
+            {
+                console.log("userdbmodule: Created table users");
+            }
+        });
+        DBobj.run(files_table_query,function(err){
+            if(err)
+            {
+                console.log("userdbmodule: Table already created");
+            }
+            else
+            {
+                console.log("userdbmodule: Created table files");
+            }
+        });
+    },
+
+    addFile:function(DBObj,filename,filetype,path,resp)
+    {
+        var query = "INSER INTO files(filename,filetype,path)VALUES(?,?,?)";
+        var stmt = DBObj.prepare(query);
+        stmt.run(filename,filetype,path,function(err)
+        {
+            if(err)
+            {
+                var uploaderr = "An error accured during the upload please try again";
+                resp.render('mainpage',{uploaderr});
+            }
+            else
+            {
+                console.log("userdbmodule: Added file %s %s %s",filename,filetype,path);
+            }
+        });
+    },
+
+
+    //DONE workover to use again
+    addUser:function(DBObj,username,userpw)
+    {
+        var stmt = DBObj.prepare("INSERT INTO users VALUES(?,?)");
+        stmt.run(username,userpw,function(err)
+        {
+            if(err){
+                console.log("userdb: failed to insert");
+                console.log(err);
+                return false;
+            }
+        });
+        stmt.finalize();
+        return true;
+    },
+    
+    //DONE
+    dropUserTable:function(DBObj){
+        var query = "DROP TABLE users";
+        DBObj.run(query,function(err){
+            if(err){
+                console.log("Failed to drop the user table with error:%s",err);
+            }
+            else
+            {
+                console.log("Succesfully dropped table users");
+            }
+        });
+    },
+    //TODO
+    deleteUser:function(DBObj,username)
+    {
+        var query = "";
+        DBobj.run(query);
+    },
+    
+    // Done needs testing tho TODO
+    handleLogin:function(DBObj,username,pass,req,resp)
+    {
+        var query = "SELECT * FROM users WHERE \"username\"= \""+username+"\"";
+        var stmt = DBObj.prepare(query);
+        stmt.get(function(err,row){
+            if(err)
+            {
+                console.log(err);
+                resp.render('login');
+            }
+            else
+            {
+                if(row == undefined)
+                {
+                    console.log("user that isnt registered tried to register")
+                    var loginerr = "Unknown username";
+                    resp.render('login',{loginerr});
+                }
+                else
+                {
+                    var db_pw = row.password
+                    console.log(db_pw);
+                    console.log(pass);
+                    if(db_pw === pass)
+                    {              
+                        console.log("userdb: login correct user exists and pass is matching");
+                        var filenames = [];
+                        fs.readdir(__FilesDir,function(err,files){
+                            files.forEach(file=>{
+                                filenames.push(file);
+                            })
+                        });
+                        resp.render('mainpage',{filenames});
+                    }
+                    else
+                    {
+                        console.log("userdb: login failed because the pass doesnt match");
+                        var loginerr = "Wrong Password";
+                        resp.render('login',{loginerr})
+                    }
+                }
+            }
+        });    
+    },
+    //TODO shit doesnt realy work out as i want it to
+    handleRegister:function(DBObj,uname,pass,req,resp)
+    {
+        var query =  "SELECT * FROM users WHERE \"username\"= \""+uname+"\"";
+        var stmt = DBObj.prepare(query)
+        var user_registered = false;
+        var val = null;
+        
+        stmt.get(function(err,row)
+        {
+            if(err)
+            {
+                console.log(err);
+                resp.render('login');
+                user_registered = true;
+            }
+            val = row;
+        });
+
+        if(!user_registered)
+        {
+            if(val == undefined)
+            {
+                var query1 = "INSERT INTO users(username,password)VALUES(?,?)";
+                var stmt1 = DBObj.prepare(query1);
+                stmt1.run(uname,pass,function(err){
+                    if(err)
+                    {
+                        console.log("userdb: failed to insert");
+                        console.log(err);
+                        resp.render('login')
+                    }
+                    else
+                    {
+                        console.log("userdb: Added user %s to the db",uname);
+                        var filenames = [];
+                        fs.readdir(__FilesDir,function(err,files){
+                            files.forEach(file=>{
+                                filenames.push(file);
+                            })
+                        });
+                        resp.render('mainpage',{filenames});
+                    }
+                });
+            }
+            else
+            {
+                console.log("userdb: User %s is already registered",uname);
+                var regerr = "Username already in use";
+                resp.render('login',{regerr});
+            }
+        }
+    },
+
+    //TODO
+    getUser:function(DBObj,username)
+    {
+        var query="";
+        DBObj
+    },
+
+    //maybe useless
+    checkCredentials:function(DBObj,username,userpw)
+    {
+        return false;
+    },
+
+    //just for debuging
+    logUsers:function(DBObj)
+    {
+        console.log("trying to log db");
+        var query = "SELECT * FROM users";
+
+        DBObj.each(query,function(err,row){
+            console.log(row.username +" "+ row.password);
+        });
+    },
+
+    //DONE
+    user_exists:function(DBObj,username)
+    {
+        var query = "SELECT * FROM users WHERE \"username\"= \""+username+"\"";
+        var stmt = DBObj.prepare(query);
+        stmt.get(function(err,row){
+            if(err)
+            {
+                console.log(err);
+                return false;
+            }
+            else
+            {
+                if(row == undefined)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        });   
+    }
+}
